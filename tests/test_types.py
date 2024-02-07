fb14c63db1fe5261c1c6aae69aaf88b2123b69ef
Tuấn-Anh Nguyễn <ubolonton@gmail.com>
2024-02-07 22:23:56 +0700

    feat: Wrap JavaScript objects/arrays by default instead of converting

--- tests/test_types.py
+++ tests/test_types.py
@@ -9,6 +9,11 @@ def runtime():
     return denopy.Runtime()
 
 
+@pytest.fixture
+def identity(runtime):
+    return runtime.eval("x => x")
+
+
 def test_numbers(runtime):
     for n in [1, 1.0, -5, -7.5]:
         assert runtime.eval(f"{n}") == n
@@ -28,9 +33,22 @@ def test_functions(runtime):
     assert js_sin(2) == math.sin(2)
 
 
-def test_roundtrips(runtime):
-    identity = runtime.eval("x => x")
+def test_roundtrips(runtime, identity):
     for v in ["abc", 1, 5.3, True, False, None,
               [], [2, 3.4, "x"],
               {}, {'a': 5, 'b': ['x', dict(c=None)]}]:
-        assert runtime.call(identity, v) == v
+        assert runtime.call(identity, v, unwrap=True) == v
+
+
+def test_objects(runtime, identity):
+    for py_object in [{}, dict(x=5)]:
+        js_value = identity(py_object)
+        assert isinstance(js_value, denopy.JsObject)
+        assert runtime.unwrap(js_value) == py_object
+
+
+def test_arrays(runtime, identity):
+    for py_object in [[], list('abc')]:
+        js_value = identity(py_object)
+        assert isinstance(js_value, denopy.JsArray)
+        assert runtime.unwrap(js_value) == py_object
